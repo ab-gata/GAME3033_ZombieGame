4c98d6ebb649df88f88010b410e9bcb0c8f59410
Pauleen Lam <pauleen.lam@outlook.com>
2022-01-19 13:10:40 -0500

    Player Inputs and Movement

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementComponent : MonoBehaviour
+{
+    [SerializeField]
+    float walkSpeed = 5;
+    [SerializeField]
+    float runSpeed = 10;
+    [SerializeField]
+    float jumpForce = 5;
+
+    // Components
+    PlayerController playerController;
+    Rigidbody rigidbody;
+
+    // Movement references
+    Vector2 inputVector = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.isJumping) return; // No air control
+        if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
+
+        moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
+        float currentSpeed = playerController.isRunning ? runSpeed : walkSpeed; // if true, do first, if false, do second
+
+        Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+
+        transform.position += movementDirection;
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+    }
+
+    public void OnRun(InputValue value)
+    {
+        playerController.isRunning = value.isPressed;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (playerController.isJumping) return;
+
+        playerController.isJumping = value.isPressed;
+        rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground") && !playerController.isJumping) return;
+
+        playerController.isJumping = false;
+    }
+}
