7250249d9bdb302b185e549f3c7161815477a501
Pauleen Lam <pauleen.lam@outlook.com>
2022-01-19 13:47:26 -0500

    Player walking animation

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -15,15 +15,22 @@ public class MovementComponent : MonoBehaviour
     // Components
     PlayerController playerController;
     Rigidbody rigidbody;
+    Animator playerAnimator;
 
     // Movement references
     Vector2 inputVector = Vector2.zero;
     Vector3 moveDirection = Vector3.zero;
 
+    public readonly int movementXHash = Animator.StringToHash("MovementX");
+    public readonly int movementYHash = Animator.StringToHash("MovementY");
+    public readonly int isJumpingHash = Animator.StringToHash("IsJumping");
+    public readonly int isRunningHash = Animator.StringToHash("IsRunning");
+
     private void Awake()
     {
         playerController = GetComponent<PlayerController>();
         rigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -35,7 +42,7 @@ public class MovementComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerController.isJumping) return; // No air control
+        //if (playerController.isJumping) return; // No air control
         if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
 
         moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
@@ -49,11 +56,18 @@ public class MovementComponent : MonoBehaviour
     public void OnMovement(InputValue value)
     {
         inputVector = value.Get<Vector2>();
+
+        // Update animation
+        playerAnimator.SetFloat(movementXHash, inputVector.x);
+        playerAnimator.SetFloat(movementYHash, inputVector.y);
     }
 
     public void OnRun(InputValue value)
     {
         playerController.isRunning = value.isPressed;
+
+        // Update animation
+        playerAnimator.SetBool(isRunningHash, playerController.isRunning);
     }
 
     public void OnJump(InputValue value)
@@ -62,6 +76,9 @@ public class MovementComponent : MonoBehaviour
 
         playerController.isJumping = value.isPressed;
         rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
+
+        // Update animation
+        playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
     }
 
     private void OnCollisionEnter(Collision collision)
