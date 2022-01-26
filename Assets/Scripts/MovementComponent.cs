a8b8d6cb4bc14a7c8c7ee15bcffa6c080ec9b068
Pauleen Lam <pauleen.lam@outlook.com>
2022-01-26 14:51:36 -0500

    Week 3 - Lab submission
    - Sprinting animations
    - Firing and reload animations with masks
    - Updated camera movement for aiming

--- Assets/Scripts/MovementComponent.cs
+++ Assets/Scripts/MovementComponent.cs
@@ -20,11 +20,19 @@ public class MovementComponent : MonoBehaviour
     // Movement references
     Vector2 inputVector = Vector2.zero;
     Vector3 moveDirection = Vector3.zero;
+    Vector2 lookInput = Vector2.zero;
+
+    // Aiming
+    public float aimSensitivity = 10;
+    [SerializeField]
+    GameObject followTarget;
 
     public readonly int movementXHash = Animator.StringToHash("MovementX");
     public readonly int movementYHash = Animator.StringToHash("MovementY");
     public readonly int isJumpingHash = Animator.StringToHash("IsJumping");
     public readonly int isRunningHash = Animator.StringToHash("IsRunning");
+    public readonly int isFiringHash = Animator.StringToHash("IsFiring");
+    public readonly int isReloadingHash = Animator.StringToHash("IsReloading");
 
     private void Awake()
     {
@@ -42,7 +50,37 @@ public class MovementComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (playerController.isJumping) return; // No air control
+        //if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Reloading"))
+        //{
+        //    // Avoid any reload.
+        //    playerController.isReloading = false;
+        //    playerAnimator.SetBool(isReloadingHash, playerController.isReloading);
+        //}
+
+        // cam xrot
+        followTarget.transform.rotation *= Quaternion.AngleAxis(lookInput.x * aimSensitivity * Time.deltaTime, Vector3.up);
+        followTarget.transform.rotation *= Quaternion.AngleAxis(lookInput.y * aimSensitivity * Time.deltaTime, Vector3.left);
+
+        var angles = followTarget.transform.localEulerAngles;
+        angles.z = 0.0f;
+
+        var angle = followTarget.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 300)
+        {
+            angles.x = 300;
+        }
+        else if(angle < 180 && angle > 70)
+        {
+            angles.x = 70;
+        }
+
+        followTarget.transform.localEulerAngles = angles;
+
+        transform.rotation = Quaternion.Euler(0, followTarget.transform.rotation.eulerAngles.y, 0);
+        followTarget.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+
+        if (playerController.isJumping) return; // No air control
         if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
 
         moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
@@ -79,6 +117,29 @@ public class MovementComponent : MonoBehaviour
 
         // Update animation
         playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
+    }
+
+    public void OnAim(InputValue value)
+    {
+        playerController.isAiming = value.isPressed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+        // aiming up/down, adjust animations, have a mask to properly animate aim
+    }
+
+    public void OnReload(InputValue value)
+    {
+        playerController.isReloading = value.isPressed;
+        playerAnimator.SetBool(isReloadingHash, playerController.isReloading);
+    }
+
+    public void OnFire(InputValue value)
+    {
+        playerController.isFiring = value.isPressed;
+        playerAnimator.SetBool(isFiringHash, playerController.isFiring);
     }
 
     private void OnCollisionEnter(Collision collision)
